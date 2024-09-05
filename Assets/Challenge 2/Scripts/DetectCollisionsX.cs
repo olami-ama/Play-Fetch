83e40d02bb8ae485f38e7b0c79625a18cafbeb16
Olami-ama <dareolamidenwamaka@outlook.com>
2024-09-05 06:55:14 -0700

    added the files

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+  
+  // detects collison between the game objects, and destroys it.
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    // cool down time in seconds
+    private float spawnCoolDown = 1.2f; 
+    // time when the player can spawn another dog
+    private float nextSpawnTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog and cooldown is passed
+        if (Input.GetKeyDown(KeyCode.Space)  && Time.time >= nextSpawnTime)
+        {
+           SpawnDog();
+            // Set the next spawn time
+            nextSpawnTime = Time.time + spawnCoolDown;
+        }
+
+    }
+      void SpawnDog()
+        {
+              Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+        }
+    
+}
